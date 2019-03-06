843cd2422f30135e392e116cea9e4f9158c6065b
Florian Böhler <florian@boehlerbrothers.com>
2019-03-06 17:49:02 +0100

    removed useless stylesheet link

--- webview.js
+++ webview.js
@@ -1,17 +1,6 @@
 import path from 'path';
 
 module.exports = (Franz) => {
-  //const getMessages = function getMessages() {
 
-    // set Franz badge
-    //Franz.setBadge(5);
-  //};
-
-  // check for new messages every second and update Franz badge
-  //Franz.loop(getMessages);
-
-  //Franz.setBadge(5);
-
-  // Hide download message
-  Franz.injectCSS(path.join(__dirname, 'service.css'));
+  //Franz.injectCSS(path.join(__dirname, 'service.css'));
 };
